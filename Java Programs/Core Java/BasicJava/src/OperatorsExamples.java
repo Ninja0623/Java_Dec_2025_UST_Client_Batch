76bab13fdb373024a1c7e6da75f6c0121f98a139
Akash <akash300383@gmail.com>
2025-12-16 19:02:01 +0530

    done

---
+++ Java Programs/Core Java/BasicJava/src/OperatorsExamples.java
@@ -0,0 +1,34 @@
+
+public class OperatorsExamples {
+
+	public static void main(String[] args) {
+//		int a=10,b=20,c=30;
+//		int sum,sub;
+//		sum = a+b;
+//		sub = a-b;
+//		System.out.println("sum of two number is "+sum);
+//		System.out.println("sub of two number is "+sub);
+//		boolean result1 = a>b;
+//		boolean result2 = a<=b;
+//		System.out.println("result1 is "+result1);
+//		System.out.println("result2 is  "+result2);
+//		
+//		boolean result3 = a >b && a >c;
+//		boolean result4 = a < b || a>c;
+//		System.out.println("result3 "+result3);
+//		System.out.println("result4 "+result4);
+		
+		int n=100;
+		System.out.println("value of n is "+n);
+		n = n+1;
+		System.out.println("value of n is "+n);
+		n++;
+		System.out.println("value of n is "+n);
+		
+		int x=1000;
+		int y=200;
+		int result5 = x>y?x:y;
+		System.out.println(result5);
+	}
+
+}
--- Java Programs/Core Java/BasicJava/src/TypeCastingExample.java
+++ Java Programs/Core Java/BasicJava/src/TypeCastingExample.java
@@ -20,7 +20,7 @@ public class TypeCastingExample {
 		System.out.println("e "+e);
 		System.out.println("f "+f);
 		//float g = (float)100.10;	// explicit type casting 
-		float g = 100.12f;			// explicit type casting 
+		float g = 100.10f;			// explicit type casting 
 		int h = (int)g;		// explicit type casting 
 		System.out.println("g "+g);
 		System.out.println("h "+h);
